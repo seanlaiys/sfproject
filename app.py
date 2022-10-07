de7d8635f3edee4f74769554d80d10d4f9291abf
Lai Yuen Sheng <laiyuensheng@Lais-MacBook-Pro-2.local>
2022-10-07 19:26:34 +0800

    Fix bug

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def get_user_url(conn, arg):
     query = ('SELECT DISTINCT t.username, t.url FROM Tweets t')
     cur.execute(query)
     urls = cur.fetchall()
-    urls = [url.replace("_", "/") for url in urls]
+    urls = [str(url).replace("_", "/") for url in urls]
     conn.close()
     return users, urls
 
@@ -65,7 +65,7 @@ def get_most_engagement_url(conn):
             ' LIMIT 20')
     cur.execute(query)
     urls = cur.fetchall()
-    urls = [url.replace("_", "/") for url in urls]
+    urls = [str(url).replace("_", "/") for url in urls]
     cur.close()
     conn.close()
     return urls
